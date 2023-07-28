9afeb68fbd54a6933efe5355ef1a63e661b9a751
Erik Garrison <erik.garrison@gmail.com>
2023-07-28 17:11:49 +0200

    rSVD crate

---
+++ src/lib.rs
@@ -0,0 +1,202 @@
+//! Randomized Singular Value Decomposition (rSVD)
+
+#![doc = include_str!("../README.md")]
+
+use ndarray::{Array2};
+use ndarray_linalg::QR;
+use ndarray_linalg::svd::SVD;
+use rand::{thread_rng, Rng};
+use rand_distr::Normal;
+
+/// Calculate a randomized SVD approximation of a matrix.
+///
+/// # Arguments
+///
+/// * `input` - The matrix to compute the randomized SVD for.
+/// * `k` - The target rank for the approximation.
+/// * `p` - The oversampling parameter.
+///
+/// # Returns
+///
+/// A tuple `(u, s, vt)` containing:
+///
+/// * `u` - The left singular vectors.
+/// * `s` - The singular values.
+/// * `vt` - The right singular vectors.
+///
+/// # Examples
+///
+/// ```
+/// use ndarray::Array2;
+/// use rsvd::rsvd;
+///
+/// let a = Array2::from_shape_vec((3, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]).unwrap();
+/// let (u, s, vt) = rsvd(&a, 2, 1);
+/// ```
+pub fn rsvd(input: &Array2<f64>, k: usize, p: usize) -> (Array2<f64>, Array2<f64>, Array2<f64>) {
+    //let m = input.shape()[0];
+    let n = input.shape()[1];
+
+    // Generate Gaussian random test matrix
+    let l = k + p; // Oversampling
+    let omega = {
+    let vec = thread_rng().sample_iter(Normal::new(0.0, 1.0).unwrap())
+        .take(l * n)
+        .collect::<Vec<_>>();
+        ndarray::Array::from_shape_vec((n, l), vec).unwrap()
+    };
+
+    // Form sample matrix Y
+    let y = input.dot(&omega);
+
+    // Orthogonalize Y 
+    let (q, _) = y.qr().unwrap();
+
+    // Project input to lower dimension
+    let b = q.t().dot(input);
+
+    // Compute SVD of small matrix B
+    let (Some(u), s, Some(vt)) = b.svd(true, true).unwrap() else {
+        panic!("SVD failed");
+    };
+
+    // Convert s to an Array2<f64>
+    let s = Array2::from_diag(&s);
+
+    // Return truncated SVD 
+    (q.dot(&u), s, vt)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::Array2;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    // input a dimension, a seed, and a tolerance
+    // we make a random matrix to match
+    // then check if the rsvd is within a tolerance of the actual svd
+    fn test_rsvd(m: usize, n:usize, k: usize, p: usize, seed: u64, tol: f64) {
+        // Generate random matrix
+        // use seeded RNG
+        let mut rng = ChaCha8Rng::seed_from_u64(seed);
+        let a = Array2::from_shape_fn((m, n), |_| rng.gen::<f64>());
+
+        // Compute rank approximation
+        let (u, s, vt) = rsvd(&a, k, p);
+
+        let (Some(u2), s2, Some(vt2)) = a.svd(true, true).unwrap()
+        else { panic!("SVD failed"); };
+
+        // convert s2 to a vector and diagonalize
+        let s2 = Array2::from_diag(&s2);
+
+        // if we have a rank k approximation, the u and u2 matrices and singular values are not comparable
+        // so we can skip the comparison and focus on vt
+        if k >= m {
+            assert!(equivalent(&u, &u2, tol));
+            assert!(equivalent(&s, &s2, tol));
+        } else {
+            //vt = vt.slice_move(s![..k, ..]);
+            //vt2 = vt2.slice_move(s![..k, ..]);
+        }
+
+        // display the matrices for each method
+        //eprintln!("u: \n{:?}", u);
+        //eprintln!("s: \n{:?}", s);
+        //eprintln!("vt: \n{:?}", vt);
+        //eprintln!("u2: \n{:?}", u2);
+        //eprintln!("s2: \n{:?}", s2);
+        //eprintln!("vt2: \n{:?}", vt2);
+
+        assert!(equivalent(&vt, &vt2, tol));
+    }
+    
+    fn equivalent(a: &Array2<f64>, b: &Array2<f64>, e: f64) -> bool {
+        let a = a.clone().mapv_into(f64::abs);
+        let b = b.clone().mapv_into(f64::abs);
+        // sum of absolute differences
+        let diff = a - b;
+        // average difference per cell
+        let avg = diff.sum() / (diff.len() as f64);
+        avg < e
+    }
+
+    // test 2x2 matrix
+    #[test]
+    fn test_rsvd_2x2() {
+        for i in 0..20 {
+            test_rsvd(2, 2, 1, 1, i, 1e-2);
+        }
+    }
+
+    // test 3x3 matrix
+    #[test]
+    fn test_rsvd_3x3() {
+        for i in 0..20 {
+            test_rsvd(3, 3, 3, 1, i, 1e-2);
+        }
+    }
+
+    // test 5x5 matrix
+    #[test]
+    fn test_rsvd_5x5() {
+        for i in 0..20 {
+            test_rsvd(5, 5, 5, 1, i, 1e-2);
+        }
+    }
+
+    // test 10x10 matrix with 5 singular values
+    #[test]
+    fn test_rsvd_10x10_k5() {
+        for i in 0..20 {
+            test_rsvd(10, 10, 5, 1, i, 0.1);
+        }
+    }
+
+    // test 10x10 matrix with 8 singular values
+    #[test]
+    fn test_rsvd_10x10_k8() {
+        for i in 0..20 {
+            test_rsvd(10, 10, 5, 1, i, 0.1);
+        }
+    }
+    
+    // test 100x100 matrix k=10
+    #[test]
+    fn test_rsvd_100x100_k10() {
+        for i in 0..20 {
+            test_rsvd(100, 100, 10, 1, i, 1e-2);
+        }
+    }
+
+    // test 100x10 matrix k=10
+    #[test]
+    fn test_rsvd_100x10_k10() {
+        for i in 0..20 {
+            test_rsvd(100, 10, 10, 1, i, 1e-2);
+        }
+    }
+    
+    // test 10x100 matrix k=10
+    #[test]
+    fn test_rsvd_10x100_k10() {
+        for i in 0..20 {
+            test_rsvd(10, 100, 10, 1, i, 1e-2);
+        }
+    }
+
+    /*
+    // test 500x500 matrix
+    #[test]
+    fn test_rsvd_500x500() {
+        for i in 0..20 {
+            test_rsvd(500, 500, 1, 1, i, 1e-2);
+        }
+}
+    */
+
+    
+}
